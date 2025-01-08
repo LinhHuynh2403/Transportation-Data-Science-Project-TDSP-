b6c9aa7441543d7ba28be64c126ce4f02c295860
Linh Huynh <128510005+LinhHuynh2403@users.noreply.github.com>
2025-01-07 22:09:35 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import folium
+GOAL: The main goal of this milestone is to set up your environment, install the required packages, learn how to access data and do some basic exploratory data analysis.
+
+Step 1: Setting up libraries and installing packages
+- A library is a collection of code that we can use in our program
+- A Package is a folder that contains libraries or other packages, organized for easy use
+
+WHAT EACH LIBRARY DOES:
+1. Pandas: is used for data manipulation and analysis
+  - Series: a one-dimension labeled array holding data of any types, such as int, string, etc
+  - DataFrame: a two-dimensional data structure that holds data like a two-dimension array with rows and columns
+2. Matplotlib: is a comprehensive library for creating static, histograms, scatter plots, animated, and interactive visualizations in Python
+3. Seaborn: is built on top of Matplotlib. It provides data visualization that are typically more aesthetic and statically sophisticated
